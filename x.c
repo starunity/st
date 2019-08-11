8118b7f50d7430cd17b4f42345ab9c27b9441b22
aleks <aleks.stier@icloud.com>
2019-08-11 03:15:26 +0200

    Make right-click paste
    Pressing right-click pastes from the primary-selection.
    If combined with the clipboard-patch right-click pastes from the clipboard.
    Middle-click does nothing.

--- x.c
+++ x.c
@@ -643,7 +643,7 @@ brelease(XEvent *e)
 		return;
 	}
 
-	if (e->xbutton.button == Button2)
+	if (e->xbutton.button == Button3)
 		selpaste(NULL);
 	else if (e->xbutton.button == Button1)
 		mousesel(e, 1);
