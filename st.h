c65bb345f0a9b037934c0287248ea49bc8f16b9e
Julius Huelsmann <juliusHuelsmann@gmail.com>
2020-07-31 10:13:30 +0200

    patch: focus

--- st.h
+++ st.h
@@ -78,6 +78,7 @@ typedef union {
 
 void die(const char *, ...);
 void redraw(void);
+void tfulldirt(void);
 void draw(void);
 
 void printscreen(const Arg *);
@@ -120,4 +121,4 @@ extern char *termname;
 extern unsigned int tabspaces;
 extern unsigned int defaultfg;
 extern unsigned int defaultbg;
-extern float alpha;
+extern float alpha, alphaUnfocused;
